93688faf6f1d258acd17c707116ac51045118a1d
Justin Zimmerman <justin.richard.zimmerman@gmail.com>
2015-07-26 16:42:22 -0400

    mid exercise13 push

--- exercise13/exercise13.js
+++ exercise13/exercise13.js
@@ -21,11 +21,11 @@ if (true) {
 if (false) {
   console.log("I never run :(");
 }
-if (prompt("How you doing?") === "sad") {
+if (prompt("How are you doing?") === "sad") {
   console.log("Well, I'm very sorry to hear that...");
 }
 if (prompt("Whats the secret number") !== "3") {
-  console.log("Wrong number!");
+  alert("Wrong number!");
 }
 
 if (prompt("Password") === "password") {
@@ -51,7 +51,7 @@ if (hero === "big") {
   if (bad_guy === "small") {
     console.log("You are no match for me!");
   }
-  console.log("A worthy opponent.");
+  console.log("Let us fight!");
 }
 
 ///////////////////////
@@ -70,6 +70,23 @@ document.querySelector('#result').textContent = response;
 document.querySelector('img').src = image_path;
 
 ////////////////////
+
+var response, image_path,
+  secret_saying = document.querySelector('#message').textContent;
+
+if (secret_saying === "The Eagles") {
+  response = "You may pass";
+  image_path = "images/open_door.png";
+} else {
+  response = "You can check-out any time you like, But you can never leave!";
+  image_path = "images/hotel_california.png";
+}
+
+document.querySelector('#result').textContent = response;
+document.querySelector('img').src = image_path;
+
+////////////////////////
+
 var user_input = document.querySelector('input#search').value,
   output_el = document.querySelector('#output');
 
@@ -85,12 +102,37 @@ if (user_input !== "happiness") {
 }
 
 /////////////////////////
+
+var user_input = document.querySelector('input#search').value,
+  output_el = document.querySelector('#output');
+
+if (user_input !== "happy") {
+  if(!user_input) {
+    document.querySelector('input').value = "hey you, type here :z";
+  } else {
+    output_el.textContent = "You're searching for the wrong thing";
+  }
+} else {
+  output_el.textContent = "ahh, the enlightend one!";
+}
+
+/////////////////////////
 // another approach
 if (user_input === "happiness") {
   output_el.textContent = "ahh, the enlightened one!";
 } else if (!user_input) {
   document.querySelector('input').value = "hey you, type here :z";
 } else {
+  output_el.textContent = "You're searching for the wrong thing";
+}
+
+//////////////////////////////
+
+if (user_input === "happiness") {
+  output_el.textContent = "ahh, the enlightend one!";
+} else if (!user_input) {
+  document.querySelector('input').value = "hey you, type here :z";
+}else {
   output_el.textContent = "You're searching for the wrong thing";
 }
 
